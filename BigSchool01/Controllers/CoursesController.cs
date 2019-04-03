b3bb777d0b94d3e8d77b6f0a1787dd77c61ca599
NgoMinhDoan_BigSchool01 <minhdoan19982000@gmail.com>
2019-04-03 12:43:17 +0700

    tạo create

--- BigSchool01/Controllers/CoursesController.cs
+++ BigSchool01/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BigSchool01.Models;
 using BigSchool01.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,28 @@ namespace BigSchool01.Controllers
             };
 
             return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- BigSchool01/ViewModels/CourseViewModel.cs
+++ BigSchool01/ViewModels/CourseViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using BigSchool01.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,23 @@ namespace BigSchool01.ViewModels
 {
     public class CourseViewModel
     {
+
+
+        [Required(ErrorMessage = "Không Được Bỏ Trống")]
+        [StringLength(250, ErrorMessage = " Không vượt qua 100 kí tự")]
         public string Place { get; set; }
+        [Required(ErrorMessage = "Không Được Bỏ Trống")]
+        [StringLength(250, ErrorMessage = " Không vượt qua 100 kí tự")]
         public string Date { get; set; }
+        [Required(ErrorMessage = "Không Được Bỏ Trống")]
+        [StringLength(250, ErrorMessage = " Không vượt qua 100 kí tự")]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+
+       
+       
 
         public DateTime GetDateTime()
         {
